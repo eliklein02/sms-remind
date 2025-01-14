5654a7cda7ac58abb69a6c39a7afb9d63dc76bba
Eli Klein <eliklein02@gmail.com>
2025-01-13 23:16:46 -0500

    yeah

--- app/jobs/reminder_job.rb
+++ app/jobs/reminder_job.rb
@@ -5,7 +5,24 @@ class ReminderJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts args[0]
-    puts args[1]
+    u_id = args[0]
+    subject = args[1]
+    send_sms(u_id, subject)
+  end
+
+  def send_sms(user_id, what)
+    u = User.find(user_id)
+    to = u.phone_number
+    what.strip!
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+    twilio_phone_number = ENV['TWILIO_PHONE_NUMBER']
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    message = @client.messages.create(
+        from: twilio_phone_number,
+        body: "Reminder: #{what}",
+        to: to
+    )
+    puts message
   end
 end
